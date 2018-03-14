3d6542d3fb280ec13e90cf8a4e5c5175356cf891
Kavinda8535 <m.p.k.dilshan@gmail.com>
2018-03-14 15:22:05 +0530

    image load fail fixed when text changed

--- src/components/pages/movies.js
+++ src/components/pages/movies.js
@@ -7,34 +7,46 @@ import { request } from 'https';
 //CROS - Cross-Origin Resource Sharing (CORS) is a W3C spec that allows cross-domain communication from the browser. By building on top of the XMLHttpRequest object.
 
 class Movies extends Component {
-  
-    constructor(){
+
+    constructor() {
         super();
         this.state = {
-            query:"trek",
-            currentMovieId: "tt2488496" // This is set for the default value in the dropdownbox.
+            query: "trek",
+            currentMovieId: "tt0796366" // This is set for the default value in the dropdownbox.
         };
+    }
+
+    search(query = this.state.query) {
+        var url = `http://www.omdbapi.com?s=${query}&y=&r=json&plot=short&&apikey=52502553`;
+        Request.get(url).then((response) => {
+            let movies = response.body.Search;
+            this.setState({
+                movies: response.body.Search,
+                total: response.body.totalResults,
+                currentMovieId: (movies && movies[0]) ? movies[0].imdbID : 'tt0796366'
+
+            });
+
+        });
     }
 
     /// ************************************************************************************************
     /// Called the first time the component is loaded right before the component is added to the page...
-    componentWillMount()
-    {
+    componentWillMount() {
 
         // console.log("componentWillMount");
         // var url = " http://www.omdbapi.com?s=star&y=&r=json&plot=short&&apikey=52502553";
         // Request.get(url).then((response)=>{
         //     this.setState({
         //         movies: response
-                
+
         //     });
         // });
     }
 
     /// **********************************************************
     /// Called after the component has been rendered into the page
-    componentDidMount()
-    {
+    componentDidMount() {
         /*
         ///
             componentDidMount is the best place to put API calls to fetch data.
@@ -45,125 +57,117 @@ class Movies extends Component {
 
         // console.log("response");
         this.search();
-        
+
     }
 
     /// componentDidUpdate() will not be invoked if shouldComponentUpdate() returns false.
     /// This method is not called for the initial render.
-    componentDidUpdate()
-    {
+    componentDidUpdate() {
         // http://www.omdbapi.com/?i=tt3896198&apikey=52502553
-        
+
     }
     /// **********************************************************
     /// Called when the props provide to the component are changed
-    componentWillReceiveProps(nextProps)
-    {
+    componentWillReceiveProps(nextProps) {
 
     }
 
     /// ****************************************
     /// Called whenthe props and/or state change
-    componentWillUpdate(nextProps, nextState)
-    {
+    componentWillUpdate(nextProps, nextState) {
 
     }
 
     /// *************************************
     /// Callled when the component is removed
-    componentWillUnmount()
-    {
+    componentWillUnmount() {
 
     }
 
-    updateSearch()
-    {
-        this.setState({query: this.refs.query.value})
-        this.search(this.refs.query.value);
-        //this.setState({currentMovieId: this.state.currentMovieId});
+    updateSearch = (e) => {
+        // this.setState({ query: this.refs.query.value })
+        this.setState({ query: e.target.value }, this.search(e.target.value));
+        //this.search({query: e.target.value});
+        //this.selectMovie();
+        // let movies = (this.state.movies) ? this.state.movies : []
+
+        //this.setState({ currentMovieId: movies[0] });
+        //this.setState({currentMovieId:this.refs.movieSelector.value})
+        //console.log("Calling updateSearch", movies[0], "ref movieSelector", this.refs.movieSelector.value);
     }
 
-    selectMovie()
-    {
-        this.setState({currentMovieId:this.refs.movieSelector.value})
-        //console.log(this.refs.movieSelector.value);
+    selectMovie() {
+        this.setState({ currentMovieId: this.refs.movieSelector.value })
+        //console.log("selectMovie",this.refs.movieSelector.value);
+        //this.setState({currentMovieId:this.refs.movieSelector.value})
     }
-  
-  render() {
-      var movies = _.map(this.state.movies, (movie) => {
-          return <li className="list-group-item" key={movie.imdbID}>{movie.Title}</li>;
-      });
 
-      var moviesOption = _.map(this.state.movies, (movie) => {
-          return <option className="last" key={`option_${movie.imdbID}`} value={movie.imdbID}>{movie.Title}</option>;
-      });
+    render() {
+        var movies = _.map(this.state.movies, (movie, i) => {
+            //console.log("movie.imdbID", movie.imdbID);trek
+            return <li className="list-group-item" key={movie.imdbID + i}>{movie.Title}</li>;
+        });
 
-      var selectedMovie = _.find(this.state. movies, (movie) => {
-          return movie.imdbID == this.state.currentMovieId;
-      });
+        var moviesOption = _.map(this.state.movies, (movie, i) => {
+            //console.log("moviesOption.imdbID", movie.imdbID);
+            return <option className="last" key={`option_${movie.imdbID + i}`} value={movie.imdbID}>{movie.Title}</option>;
+        });
 
-      var img;
-      if(selectedMovie){
-          img= <img src={selectedMovie.Poster} />
-      }
+        var selectedMovie = _.find(this.state.movies, (movie) => {
+            //console.log("selectedMovie",this.state.currentMovieId, "imdID -", movie.imdbID , "movie.imdbID == this.state.currentMovieId", movie.imdbID == this.state.currentMovieId)
 
-    return (
-        
-        <div className="container-fluid">
+            return movie.imdbID == this.state.currentMovieId;
+        });
 
-            <div className="row">
+        var img;
+        //console.log("img selectedMovie",selectedMovie )
+        if (selectedMovie) {
+            img = <img src={selectedMovie.Poster} />
+        }
 
-                <h1>
-                    Movies Search here.
+        return (
+
+            <div className="container-fluid">
+
+                <div className="row">
+
+                    <h1>
+                        Movies Search here
             </h1>
-            </div>
-
-            <div className="row">
-
-                <div className="col-sm">
-                    <div className="mb-2">
-                        {/* <input ref="query" onChange={ (e) => {this.updateSearch();}} type="text" defaultValue="matrix" value={this.state.query}/> */}
-                        <input ref="query" onChange={(e) => { this.updateSearch(); }} type="text" value={this.state.query} />
-                        <ul className="list-group list-group-flush">{movies}</ul>
-                    </div>
-
                 </div>
 
-                <div className="col-sm">
+                <div className="row">
 
-                    <div className="row mb-2 ml-3">
-                        <select ref="movieSelector" value={this.state.currentMovieId} onChange={(e) => { this.selectMovie(); }}> {moviesOption}</select>
+                    <div className="col-sm">
+                        <div className="mb-2">
+                            {/* <input ref="query" onChange={ (e)now => {this.updateSearch();}} type="text" defaultValue="matrix" value={this.state.query}/> */}
+                            <input ref="query" onChange={(e) => { this.updateSearch(e); }} type="text" value={this.state.query} />
+                            <ul className="list-group list-group-flush">{movies}</ul>
+                        </div>
+
                     </div>
 
                     <div className="col-sm">
-                        {img}
-                    </div>
+    
+                    <div className="row mb-2 ml-3">
+                            <select ref="movieSelector" value={this.state.currentMovieId} onChange={(e) => { this.selectMovie(); }}> {moviesOption}</select>
+                        </div>
+                        <div className="col-sm">
+                            {img}
+                        </div>
 
 
-                    <div className="row mt-2 ml-3">
-                        <button type="button" className="btn btn-primary">Search</button>
+                        <div className="row mt-2 ml-3">
+                            <button type="button" className="btn btn-primary">Search</button>
+                        </div>
+
                     </div>
 
                 </div>
-
             </div>
-        </div>
-        
-    );
-  }
 
-  search(query = "star")
-  {
-    var url = `http://www.omdbapi.com?s=${query}&y=&r=json&plot=short&&apikey=52502553`;
-    Request.get(url).then((response)=>{
-        this.setState({
-            movies: response.body.Search,
-            total: response.body.totalResults
-            
-        });
-        
-    });
-  }
+        );
+    }
 }
 
 export default Movies;
