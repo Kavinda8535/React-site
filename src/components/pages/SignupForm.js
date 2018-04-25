ee37f57fdce7fdf6dbc9a0471b9b50efb240825c
Kavinda8535 <m.p.k.dilshan@gmail.com>
2018-04-25 11:45:47 +0530

    adding passsword component for SighupForm

--- src/components/pages/SignupForm.js
+++ src/components/pages/SignupForm.js
@@ -2,6 +2,43 @@ import React, { Component } from 'react';
 import timezones from '../data/timezones';
 import map from 'lodash/map';
 
+export class Password extends Component
+{
+    constructor(props){
+        super(props);
+        this.state = {
+        repwtype: 'input'
+        }
+        this.showHide = this.showHide.bind(this);
+    }
+
+    showHide(e)
+    {
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({
+            //type: this.state.type === 'input' ? 'password' : 'input',
+            repwtype: this.state.repwtype === 'input' ? 'password' : 'input'
+        })
+    }
+
+render
+(){
+   return(
+    <div className="input-group">
+    {/* {console.log("retype", this.state.repwtype)} */}
+        <input value={this.state.passwordConfirmation} onChange={this.onChange} type={this.state.repwtype} name="passwordConfirmation" className="form-control" placeholder="Re-enter password" />
+        <span className="input-group-btn">
+            <button className="btn btn-default reveal" type="button" onClick={this.showHide}>{this.state.repwtype === 'input' ? 'Hide' : 'Show'}<i className="glyphicon glyphicon-eye-open"></i></button>
+        </span>
+    </div>
+   )               
+
+}
+
+
+}
+
 class SighupForm extends Component{
     constructor(props){
         super(props);
@@ -10,11 +47,42 @@ class SighupForm extends Component{
             email: '',
             password: '',
             passwordConfirmation:'',
-            timeZone: ''
+            timeZone: '',
+            type: 'input',
+            score: 'null'
         }
         this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);        
+        this.onSubmit = this.onSubmit.bind(this);
+        this.showHide = this.showHide.bind(this);
+        //this.passwordStrength = this.passwordStrength.bind(this);
     }
+
+    showHide(e)
+    {
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({
+            //type: this.state.type === 'input' ? 'password' : 'input',
+            repwtype: this.state.repwtype === 'input' ? 'password' : 'input'
+        })
+    }
+
+    // passwordStrength(e)
+    // {
+    //     if(e.target.value === '')
+    //     {
+    //         this.setState({
+    //             score: 'null'
+    //         })
+    //     }
+    //     else
+    //     {
+    //         var pw = zxcvbn(e.target.value);
+    //         this.setState({
+    //             score: pw.score
+    //         });
+    //     }
+    // }
 
     onChange(e)
     {
@@ -37,25 +105,42 @@ class SighupForm extends Component{
 
                 <div className="form-group">
                     <label className="controle-label">Username</label>
-                    <input value={this.state.username} onChange={this.onChange} type="text" name="username" className="form-control"/>
+                    <input value={this.state.username} onChange={this.onChange} type="text" name="username" className="form-control" placeholder="ABC123@gmail.com"/>
 
                 </div>
 
                 <div className="form-group">
                     <label className="controle-label">Email</label>
-                    <input value={this.state.email} onChange={this.onChange} type="text" name="email" className="form-control"/>
+                    <input value={this.state.email} onChange={this.onChange} type="text" name="email" className="form-control" placeholder="Email Address"/>
 
                 </div>
 
                 <div className="form-group">
                     <label className="controle-label">Password</label>
-                    <input value={this.state.password} onChange={this.onChange} type="text" name="password" className="form-control"/>
+                    <Password/>
+
+                    {/* <div className="input-group">
+                        <input value={this.state.password} onChange={this.onChange} type={this.state.type} name="password" className="form-control" placeholder="Enter password">
+                        </input>
+                     
+                        <span className="input-group-btn">
+                            <button className="btn btn-default reveal" type="button" onClick={this.showHide}>{this.state.type === 'input' ? 'Hide' : 'Show'}<i className="glyphicon glyphicon-eye-open"></i></button>
+                        </span>
+                    </div> */}
+                    
 
                 </div>
 
                 <div className="form-group">
                     <label className="controle-label">Password Confirmation</label>
-                    <input value={this.state.passwordConfirmation} onChange={this.onChange} type="text" name="passwordConfirmation" className="form-control"/>
+                    <Password/>
+                    {/* <div className="input-group">
+                    {console.log("retype", this.state.repwtype)}
+                        <input value={this.state.passwordConfirmation} onChange={this.onChange} type="password" name="passwordConfirmation" className="form-control" placeholder="Re-enter password" />
+                        <span className="input-group-btn">
+                            <button className="btn btn-default reveal" type="button" onClick={this.showHide}>{this.state.repwtype === 'input' ? 'Hide' : 'Show'}<i className="glyphicon glyphicon-eye-open"></i></button>
+                        </span>
+                    </div> */}
 
                 </div>
 
