f3f51c620d4e55fe6a0b5202268dc648e730540b
Kavinda8535 <m.p.k.dilshan@gmail.com>
2018-07-02 17:28:33 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,11 @@
 
   // Initialize Firebase
   export const DB_CONFIG = {
-    apiKey: "AIzaSyATJ7-B8Il8BmmdpkTvbZ3_Yw7oT6JU9EI",
-    authDomain: "myreactreduxapp.firebaseapp.com",
-    databaseURL: "https://myreactreduxapp.firebaseio.com",
-    projectId: "myreactreduxapp",
-    storageBucket: "myreactreduxapp.appspot.com",
-    messagingSenderId: "70174681669"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   
